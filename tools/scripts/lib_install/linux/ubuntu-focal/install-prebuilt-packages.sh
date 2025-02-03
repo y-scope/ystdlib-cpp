d1ff6d15be1eb33f6d9d5cdf2a5fa89dc55c0ef2
Bingran Hu <bingran.hu@yscope.com>
2025-02-03 18:45:42 -0500

    Add more sudo

--- tools/scripts/lib_install/linux/ubuntu-focal/install-prebuilt-packages.sh
+++ tools/scripts/lib_install/linux/ubuntu-focal/install-prebuilt-packages.sh
@@ -24,8 +24,8 @@ DEBIAN_FRONTEND=noninteractive ${privileged_command_prefix} apt-get install \
 # Add alternatives for cpp-10, gcc-10, and g++-10
 # NOTE: We use a low priority to avoid affecting the prioritization of any existing alternatives
 # on the user's system.
-update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-10 0 \
+${privileged_command_prefix} update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-10 0 \
   --slave /usr/share/man/man1/cc.1.gz cc.1.gz /usr/share/man/man1/gcc.1.gz
-update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-10 0 \
+${privileged_command_prefix} update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-10 0 \
   --slave /usr/share/man/man1/c++.1.gz c++.1.gz /usr/share/man/man1/g++.1.gz
-update-alternatives --install /lib/cpp cpp /usr/bin/cpp-10 0
+${privileged_command_prefix} update-alternatives --install /lib/cpp cpp /usr/bin/cpp-10 0
