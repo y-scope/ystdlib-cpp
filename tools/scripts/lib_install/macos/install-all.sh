fa3eb4f089df3a788361596be8e659e5b5e05364
Bingran Hu <bingran.hu@yscope.com>
2025-02-04 07:07:20 -0500

    test

---
+++ tools/scripts/lib_install/macos/install-all.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Exit on any error
+set -e
+
+# Error on undefined variable
+set -u
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+echo "Checking for elevated privileges..."
+privileged_command_prefix=""
+USER_ID=${EUID:-$(id -u)}
+if [[ 0 -ne ${USER_ID} ]] ; then
+  sudo echo "Script can elevate privileges."
+  privileged_command_prefix="${privileged_command_prefix} sudo"
+fi
+
+# Check if the system is running Ubuntu and determine the version
+if [[ -f /etc/os-release ]]; then
+  source /etc/os-release
+  if [[ "$ID" == "ubuntu" &&
+      ( "${VERSION_CODENAME}" == "focal" ||
+        "${VERSION_CODENAME}" == "jammy" ||
+        "${VERSION_CODENAME}" == "noble" ) ]]; then
+    dist_dir="${script_dir}/ubuntu-${VERSION_CODENAME}"
+    for exe_file in \
+        "${dist_dir}/install-prebuilt-packages.sh" "${dist_dir}/install-packages-from-source.sh"; do
+	  [[ -f "${exe_file}"  ]] && ${privileged_command_prefix} "${exe_file}"
+    done
+    exit 0
+  fi
+fi
+
+echo "Supports only Ubuntu LTS releases from Ubuntu 20.04 LTS onward." >&2
+exit 1
