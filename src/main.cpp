0c77022884ea46e16069b7392a7ebe68dcb96301
Bingran Hu <bingran.hu@yscope.com>
2025-02-03 22:18:27 -0500

    Add c++20 lines to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <vector>
 #include <ystdlib/testlib/hello.hpp>
 
 [[nodiscard]] auto main() -> int {
+    constexpr std::vector<int> cV;
     std::cout << ystdlib::testlib::hello() << '\n';
     return 0;
 }
