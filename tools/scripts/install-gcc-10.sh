48c8e9ba22a06f78e560e56757e3b1216889948c
Bingran Hu <bingran.hu@yscope.com>
2025-02-04 20:03:45 -0500

    Update alternatives

--- tools/scripts/install-gcc-10.sh
+++ tools/scripts/install-gcc-10.sh
@@ -12,3 +12,12 @@ DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
     g++-10 \
     gcc \
     gcc-10
+
+# Add alternatives for cpp-10, gcc-10, and g++-10
+# NOTE: We use a low priority to avoid affecting the prioritization of any existing alternatives
+# on the user's system.
+update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-10 0 \
+  --slave /usr/share/man/man1/cc.1.gz cc.1.gz /usr/share/man/man1/gcc.1.gz
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-10 0 \
+  --slave /usr/share/man/man1/c++.1.gz c++.1.gz /usr/share/man/man1/g++.1.gz
+update-alternatives --install /lib/cpp cpp /usr/bin/cpp-10 0
