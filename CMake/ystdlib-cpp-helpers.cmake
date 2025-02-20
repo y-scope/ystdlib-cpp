37f1b515ec69c06e244b5e9b2c18610611302589
Bingran Hu <bingran.hu@yscope.com>
2025-02-19 21:23:59 -0500

    Add helper function to build unittest

--- CMake/ystdlib-cpp-helpers.cmake
+++ CMake/ystdlib-cpp-helpers.cmake
@@ -33,3 +33,29 @@ function(cpp_library)
     target_compile_features(${arg_cpp_lib_NAME} INTERFACE cxx_std_20)
     add_library(${arg_cpp_lib_NAMESPACE}::${arg_cpp_lib_NAME} ALIAS ${arg_cpp_lib_NAME})
 endfunction()
+
+# Adds the current C++ library's unit test source files and dependencies to global list variables
+# used to build the Catch2 unit test target.
+# NOTE: There's no need to specify Catch2 related dependencies.
+#
+# @param SRCS
+# @param DEPS
+function(cpp_test)
+    set(options "")
+    set(oneValueArgs "")
+    set(multiValueArgs
+        SRCS
+        DEPS
+    )
+    cmake_parse_arguments(arg_cpp_test "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    file(RELATIVE_PATH SRC_DIR "${PROJECT_SOURCE_DIR}" "${CMAKE_CURRENT_LIST_DIR}")
+
+    foreach(SRC_FILE IN LISTS arg_cpp_test_SRCS)
+        list(APPEND SOURCE_FILES_unitTest ${SRC_DIR}/${SRC_FILE})
+    endforeach()
+    set(SOURCE_FILES_unitTest "${SOURCE_FILES_unitTest}" CACHE INTERNAL "")
+
+    list(APPEND LIB_DEPS_unitTest "${arg_cpp_test_DEPS}")
+    set(LIB_DEPS_unitTest "${LIB_DEPS_unitTest}" CACHE INTERNAL "")
+endfunction()
