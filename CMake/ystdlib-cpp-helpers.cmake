9012991b46c4435e59ee87b4a9de635830198759
Bingran Hu <bingran.hu@yscope.com>
2025-02-20 00:22:12 -0500

    Redo the global unit test target and add library-specific unit tests

--- CMake/ystdlib-cpp-helpers.cmake
+++ CMake/ystdlib-cpp-helpers.cmake
@@ -1,8 +1,12 @@
 # Adds a c++20 interface library in the subdirectory NAME with the target NAME and alias
 # NAMESPACE::NAME. Libraries with multiple levels of namespace nesting are currently not supported.
 #
+# If `BUILD_TESTING` is ON, build the unit tests for this library only, and link this library
+# against the global unit test target for the entire `ystdlib-cpp`.
+#
 # @param NAME
 # @param NAMESPACE
+# @parms TESTS
 # @param [LIB_BUILD_INTERFACE="${PROJECT_SOURCE_DIR}/src"] The list of include paths for building
 # the library and for external projects that link against it via the add_subdirectory() function.
 function(cpp_library)
@@ -11,7 +15,10 @@ function(cpp_library)
         NAME
         NAMESPACE
     )
-    set(multiValueArgs LIB_BUILD_INTERFACE)
+    set(multiValueArgs
+        TESTS
+        LIB_BUILD_INTERFACE
+    )
     cmake_parse_arguments(arg_cpp_lib "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     # TODO: Turn this into a function for handling other optional params that have default values.
@@ -24,6 +31,7 @@ function(cpp_library)
         set(arg_cpp_lib_LIB_BUILD_INTERFACE "${PROJECT_SOURCE_DIR}/src")
     endif()
 
+    # Build interface library
     add_library(${arg_cpp_lib_NAME} INTERFACE)
     target_include_directories(
         ${arg_cpp_lib_NAME}
@@ -32,30 +40,26 @@ function(cpp_library)
     )
     target_compile_features(${arg_cpp_lib_NAME} INTERFACE cxx_std_20)
     add_library(${arg_cpp_lib_NAMESPACE}::${arg_cpp_lib_NAME} ALIAS ${arg_cpp_lib_NAME})
-endfunction()
 
-# Adds the current C++ library's unit test source files and dependencies to global list variables
-# used to build the Catch2 unit test target.
-# NOTE: There's no need to specify Catch2 related dependencies.
-#
-# @param SRCS
-# @param DEPS
-function(cpp_test)
-    set(options "")
-    set(oneValueArgs "")
-    set(multiValueArgs
-        SRCS
-        DEPS
-    )
-    cmake_parse_arguments(arg_cpp_test "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+    if(BUILD_TESTING)
+        # Build the unit test target specific to the current library.
+        set(_UNIT_TEST_TARGET "unit-test-binary-${arg_cpp_lib_NAME}")
+        add_executable(${_UNIT_TEST_TARGET})
+        target_sources(${_UNIT_TEST_TARGET} PRIVATE ${arg_cpp_lib_TESTS})
+        target_link_libraries(
+            ${_UNIT_TEST_TARGET}
+            PRIVATE
+                Catch2::Catch2WithMain
+                ${arg_cpp_lib_NAMESPACE}::${arg_cpp_lib_NAME}
+        )
+        target_compile_features(${_UNIT_TEST_TARGET} PRIVATE cxx_std_20)
 
-    file(RELATIVE_PATH SRC_DIR "${PROJECT_SOURCE_DIR}" "${CMAKE_CURRENT_LIST_DIR}")
-
-    foreach(SRC_FILE IN LISTS arg_cpp_test_SRCS)
-        list(APPEND SOURCE_FILES_unitTest ${SRC_DIR}/${SRC_FILE})
-    endforeach()
-    set(SOURCE_FILES_unitTest "${SOURCE_FILES_unitTest}" CACHE INTERNAL "")
-
-    list(APPEND LIB_DEPS_unitTest "${arg_cpp_test_DEPS}")
-    set(LIB_DEPS_unitTest "${LIB_DEPS_unitTest}" CACHE INTERNAL "")
+		# Link the current library against the global unit test target.
+        target_sources(${GLOBAL_UNIT_TEST_TARGET} PRIVATE ${arg_cpp_lib_TESTS})
+        target_link_libraries(
+            ${GLOBAL_UNIT_TEST_TARGET}
+            PRIVATE
+                ${arg_cpp_lib_NAMESPACE}::${arg_cpp_lib_NAME}
+        )
+    endif()
 endfunction()
