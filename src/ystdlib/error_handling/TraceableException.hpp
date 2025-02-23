518f01832370890daeddc00eeffd382a3fb0fe44
Bingran Hu <bingran.hu@yscope.com>
2025-02-23 10:40:59 -0500

    Refactor Traceable Exception

---
+++ src/ystdlib/error_handling/TraceableException.hpp
@@ -0,0 +1,63 @@
+#ifndef YSTDLIB_ERROR_HANDLING_TRACEABLEEXCEPTION_HPP
+#define YSTDLIB_ERROR_HANDLING_TRACEABLEEXCEPTION_HPP
+
+#include <exception>
+#include <source_location>
+#include <system_error>
+
+namespace ystdlib::error_handling {
+/**
+ * An exception class that is thrown with an `std::error_code`.
+ *
+ * This class extends `std::exception` and can be thrown with an `std::error_code` argument. It also
+ * provides additional information to aid in debugging by storing details in `std::source_location`,
+ * including the function name, file name, and line number of the throwing location.
+ *
+ * @see std::source_location::file_name()
+ * @see std::source_location::function_name()
+ * @see std::source_location::line()
+ */
+class TraceableException : public std::exception, public std::source_location {
+public:
+    // Constructor
+    TraceableException(
+            std::error_code error_code,
+            std::source_location const& location = std::source_location::current()
+    )
+            : std::source_location{location},
+              m_error_code(error_code) {
+        m_what = std::string{function_name()} + " operation failed.";
+    }
+
+    // Methods
+    [[nodiscard]] auto error_code() const -> std::error_code { return m_error_code; }
+
+#ifdef SOURCE_PATH_SIZE
+    [[nodiscard]] auto file_name() const -> char const* {
+        // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        return std::source_location::file_name() + SOURCE_PATH_SIZE;
+    }
+#endif
+
+    // Methods implementing std::exception
+    [[nodiscard]] auto what() const noexcept -> char const* override { return m_what.c_str(); }
+
+private:
+    // Variables
+    std::error_code m_error_code;
+    std::string m_what;
+};
+
+}  // namespace ystdlib::error_handling
+
+/**
+ * The macro to define a `TraceableException` class with the given class name T.
+ */
+// NOLINTBEGIN(bugprone-macro-parentheses, cppcoreguidelines-macro-usage)
+#define YSTDLIB_ERROR_HANDLING_DEFINE_TRACEABLE_EXCEPTION(T) \
+    class T : public ystdlib::error_handling::TraceableException { \
+        using ystdlib::error_handling::TraceableException::TraceableException; \
+    }
+// NOLINTEND(bugprone-macro-parentheses, cppcoreguidelines-macro-usage)
+
+#endif  // YSTDLIB_ERROR_HANDLING_TRACEABLEEXCEPTION_HPP
