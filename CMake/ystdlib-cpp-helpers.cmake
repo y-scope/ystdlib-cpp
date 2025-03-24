87ce4705d30624d6b6e353cf1bb1b863777a040d
Bingran Hu <bingran.hu@yscope.com>
2025-03-24 12:33:30 -0400

    Add add_lib_include_interface_target

--- CMake/ystdlib-cpp-helpers.cmake
+++ CMake/ystdlib-cpp-helpers.cmake
@@ -14,6 +14,43 @@ function(check_if_header_only SOURCE_LIST IS_HEADER_ONLY NON_HEADER_FILE)
     set(${NON_HEADER_FILE} "" PARENT_SCOPE)
 endfunction()
 
+# Defines a CMake interface target to expose the include path resolution directory of a third-party
+# library.
+
+# The created target can be linked against `ystdlib-cpp` libraries to ensure
+# consistent header resolution across dependent projects.
+#
+# This is useful for third-party libraries that do not properly configure target-based include paths
+# using `target_include_directories($<INSTALL_INTERFACE>)`.
+#
+# Call this function after using the `find_package()` call so that `<LIB_NAME>_ROOT` is defined.
+#
+# @param LIB_NAME     The base name of the library (expects <LIB_NAME>_ROOT to be defined).
+# @param [DEP_LIST]   Optional list of interface dependencies (e.g., nested include targets).
+function(add_lib_include_interface_target)
+    set(options "")
+    set(oneValueArgs LIB_NAME)
+    set(multiValueArgs DEP_LIST)
+    cmake_parse_arguments(arg "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    # Extract path to package library installation
+    set(_LIB_ROOT_PATH_VAR "${arg_LIB_NAME}_ROOT")
+    if(NOT DEFINED ${_LIB_ROOT_PATH_VAR})
+        message(FATAL_ERROR "${arg_LIB_NAME} install path `${_LIB_ROOT_PATH_VAR}` is not defined.")
+    endif()
+    set(_LIB_ROOT_PATH "${${_LIB_ROOT_PATH_VAR}}")
+
+    set(_TARGET_NAME "${arg_LIB_NAME}_include")
+    add_library(${_TARGET_NAME} INTERFACE)
+    add_library(${arg_LIB_NAME}::${_TARGET_NAME} ALIAS ${_TARGET_NAME})
+    target_include_directories(
+        ${_TARGET_NAME}
+        INTERFACE
+            "$<BUILD_INTERFACE:${_LIB_ROOT_PATH}/include>"
+    )
+    target_link_libraries(${_TARGET_NAME} INTERFACE ${arg_DEP_LIST})
+endfunction()
+
 # Adds a c++20 interface library in the subdirectory NAME with the target NAME and alias
 # NAMESPACE::NAME. Libraries with multiple levels of namespace nesting are currently not supported.
 #
@@ -65,12 +102,19 @@ function(cpp_library)
 
     check_if_header_only(arg_cpp_lib_PRIVATE_SOURCES _IS_INTERFACE_LIB _)
     if(_IS_INTERFACE_LIB)
+        if(arg_cpp_lib_PRIVATE_LINK_LIBRARIES)
+            message(
+                FATAL_ERROR
+                "Private dependency specifications disabled for interface library target ${_ALIAS_TARGET_NAME}."
+            )
+        endif()
         add_library(${arg_cpp_lib_NAME} INTERFACE)
         target_include_directories(
             ${arg_cpp_lib_NAME}
             INTERFACE
                 "$<BUILD_INTERFACE:${arg_cpp_lib_BUILD_INCLUDE_DIR}>"
         )
+        target_link_libraries(${arg_cpp_lib_NAME} INTERFACE ${arg_cpp_lib_PUBLIC_LINK_LIBRARIES})
         target_compile_features(${arg_cpp_lib_NAME} INTERFACE cxx_std_20)
     else()
         # The library type is specified by `BUILD_SHARED_LIBS` if it is defined. Otherwise, the type
@@ -87,16 +131,16 @@ function(cpp_library)
             PUBLIC
                 "$<BUILD_INTERFACE:${arg_cpp_lib_BUILD_INCLUDE_DIR}>"
         )
+        target_link_libraries(
+            ${arg_cpp_lib_NAME}
+            PUBLIC
+                ${arg_cpp_lib_PUBLIC_LINK_LIBRARIES}
+            PRIVATE
+                ${arg_cpp_lib_PRIVATE_LINK_LIBRARIES}
+        )
         target_compile_features(${arg_cpp_lib_NAME} PUBLIC cxx_std_20)
     endif()
 
-    target_link_libraries(
-        ${arg_cpp_lib_NAME}
-        PUBLIC
-            ${arg_cpp_lib_PUBLIC_LINK_LIBRARIES}
-        PRIVATE
-            ${arg_cpp_lib_PRIVATE_LINK_LIBRARIES}
-    )
     add_library(${_ALIAS_TARGET_NAME} ALIAS ${arg_cpp_lib_NAME})
 
     if(YSTDLIB_CPP_ENABLE_TESTS)
