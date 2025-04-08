3555c5bf61ca40f30b32474db20a33716bf53bd9
Bingran Hu <bingran.hu@yscope.com>
2025-04-08 02:33:16 -0400

    Add time types

---
+++ src/ystdlib/wrapped_facade_headers/bits/types/clockid_t.h
@@ -0,0 +1,14 @@
+#ifndef YSTDLIB_WRAPPED_FACADE_HEADERS_BITS_TYPES_CLOCKID_T_HPP
+#define YSTDLIB_WRAPPED_FACADE_HEADERS_BITS_TYPES_CLOCKID_T_HPP
+
+// clang-format off
+// IWYU pragma: begin_exports
+#include <bits/types/clockid_t.h>
+
+#if defined(__APPLE__)
+#include <_time.h>
+#endif
+// IWYU pragma: end_exports
+// clang-format on
+
+#endif  // YSTDLIB_WRAPPED_FACADE_HEADERS_BITS_TYPES_CLOCK_T_HPP
---
+++ src/ystdlib/wrapped_facade_headers/bits/types/time_t.h
@@ -0,0 +1,14 @@
+#ifndef YSTDLIB_WRAPPED_FACADE_HEADERS_BITS_TYPES_TIME_T_HPP
+#define YSTDLIB_WRAPPED_FACADE_HEADERS_BITS_TYPES_TIME_T_HPP
+
+// clang-format off
+// IWYU pragma: begin_exports
+#include <bits/types/time_t.h>
+
+#if defined(__APPLE__)
+#include <sys/_types/_time_t.h>
+#endif
+// IWYU pragma: end_exports
+// clang-format on
+
+#endif  // YSTDLIB_WRAPPED_FACADE_HEADERS_BITS_TYPES_TIME_T_HPP
