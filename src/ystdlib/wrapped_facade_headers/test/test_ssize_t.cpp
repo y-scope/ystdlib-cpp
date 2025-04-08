6300c50b3c8770de4a0f01a947531d891fe05bcc
Bingran Hu <bingran.hu@yscope.com>
2025-04-07 22:01:30 -0400

    Add ssize_t

---
+++ src/ystdlib/wrapped_facade_headers/test/test_ssize_t.cpp
@@ -0,0 +1,10 @@
+#include <unistd.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace ystdlib::wrapped_facade_headers::test {
+TEST_CASE("test_ssize_t", "[wrapped_facade_headers][ssize_t]") {
+    ssize_t const i{0};
+    REQUIRE((0 == i));
+}
+}  // namespace ystdlib::wrapped_facade_headers::test
