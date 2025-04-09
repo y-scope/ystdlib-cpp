d3fc9804eacb3ee4f156ae0ca37151cb04847580
Bingran Hu <bingran.hu@yscope.com>
2025-04-09 16:25:19 -0400

    feat(wrapped_facade_headers): Expand `sys/types.h` facade to cover all Linux Glibc typedefs that are also present in macOS. (#53)

--- src/ystdlib/wrapped_facade_headers/sys/types.h
+++ src/ystdlib/wrapped_facade_headers/sys/types.h
@@ -1,9 +1,40 @@
+#ifndef YSTDLIB_WRAPPED_FACADE_HEADERS_SYS_TYPES_HPP
+#define YSTDLIB_WRAPPED_FACADE_HEADERS_SYS_TYPES_HPP
+
 // clang-format off
 // IWYU pragma: begin_exports
 #include <sys/types.h>
 
 #if defined(__APPLE__)
+#include <sys/_types/_u_char.h>
+#include <sys/_types/_u_short.h>
+#include <sys/_types/_u_int.h>
+#include <sys/_types/_fsid_t.h>
+#include <sys/_types/_ino_t.h>
+#include <sys/_types/_ino64_t.h>
+#include <sys/_types/_dev_t.h>
+#include <sys/_types/_gid_t.h>
+#include <sys/_types/_mode_t.h>
+#include <sys/_types/_nlink_t.h>
+#include <sys/_types/_uid_t.h>
 #include <sys/_types/_off_t.h>
+#include <sys/_types/_pid_t.h>
+#include <sys/_types/_id_t.h>
+#include <sys/_types/_ssize_t.h>
+#include <sys/_types/_caddr_t.h>
+#include <sys/_types/_key_t.h>
+#include <sys/_types/_useconds_t.h>
+#include <sys/_types/_suseconds_t.h>
+#include <sys/_types/_u_int8_t.h>
+#include <sys/_types/_u_int16_t.h>
+#include <sys/_types/_u_int32_t.h>
+#include <sys/_types/_u_int64_t.h>
+#include <sys/_types/_blksize_t.h>
+#include <sys/_types/_blkcnt_t.h>
+#include <sys/_types/_fsblkcnt_t.h>
+#include <sys/_types/_fsfilcnt_t.h>
 #endif
 // IWYU pragma: end_exports
 // clang-format on
+
+#endif  // YSTDLIB_WRAPPED_FACADE_HEADERS_SYS_TYPES_HPP
---
+++ src/ystdlib/wrapped_facade_headers/test/test_sys_types.cpp
@@ -0,0 +1,166 @@
+#include <ystdlib/wrapped_facade_headers/sys/types.h>
+
+#include <catch2/catch_test_macros.hpp>
+
+namespace ystdlib::wrapped_facade_headers::test {
+TEST_CASE("test_sys_types_u_char", "[wrapped_facade_headers][sys_types][u_char]") {
+    u_char const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_u_short", "[wrapped_facade_headers][sys_types][u_short]") {
+    u_short const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_u_int", "[wrapped_facade_headers][sys_types][u_int]") {
+    u_int const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_u_long", "[wrapped_facade_headers][sys_types][u_long]") {
+    u_long const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_quad_t", "[wrapped_facade_headers][sys_types][quad_t]") {
+    quad_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_u_quad_t", "[wrapped_facade_headers][sys_types][u_quad_t]") {
+    u_quad_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_fsid_t", "[wrapped_facade_headers][sys_types][fsid_t]") {
+    fsid_t const i{{0, 0}};
+#if defined(__linux__)
+    REQUIRE(0 == i.__val[0]);
+    REQUIRE(0 == i.__val[1]);
+#elif defined(__APPLE__)
+    REQUIRE(0 == i.val[0]);
+    REQUIRE(0 == i.val[1]);
+#endif
+}
+
+TEST_CASE("test_sys_types_ino_t", "[wrapped_facade_headers][sys_types][ino_t]") {
+    ino_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_ino64_t", "[wrapped_facade_headers][sys_types][ino64_t]") {
+    ino64_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_dev_t", "[wrapped_facade_headers][sys_types][dev_t]") {
+    dev_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_gid_t", "[wrapped_facade_headers][sys_types][gid_t]") {
+    gid_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_mode_t", "[wrapped_facade_headers][sys_types][mode_t]") {
+    mode_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_nlink_t", "[wrapped_facade_headers][sys_types][nlink_t]") {
+    nlink_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_uid_t", "[wrapped_facade_headers][sys_types][uid_t]") {
+    uid_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_off_t", "[wrapped_facade_headers][sys_types][off_t]") {
+    off_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_pid_t", "[wrapped_facade_headers][sys_types][pid_t]") {
+    pid_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_id_t", "[wrapped_facade_headers][sys_types][id_t]") {
+    id_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_ssize_t", "[wrapped_facade_headers][sys_types][ssize_t]") {
+    ssize_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_daddr_t", "[wrapped_facade_headers][sys_types][daddr_t]") {
+    daddr_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_caddr_t", "[wrapped_facade_headers][sys_types][caddr_t]") {
+    caddr_t p{nullptr};
+    REQUIRE(nullptr == p);
+}
+
+TEST_CASE("test_sys_types_key_t", "[wrapped_facade_headers][sys_types][key_t]") {
+    key_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_useconds_t", "[wrapped_facade_headers][sys_types][useconds_t]") {
+    useconds_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_suseconds_t", "[wrapped_facade_headers][sys_types][suseconds_t]") {
+    suseconds_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_u_int8_t", "[wrapped_facade_headers][sys_types][u_int8_t]") {
+    u_int8_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_u_int16_t", "[wrapped_facade_headers][sys_types][u_int16_t]") {
+    u_int16_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_u_int32_t", "[wrapped_facade_headers][sys_types][u_int32_t]") {
+    u_int32_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_u_int64_t", "[wrapped_facade_headers][sys_types][u_int64_t]") {
+    u_int64_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_blksize_t", "[wrapped_facade_headers][sys_types][blksize_t]") {
+    blksize_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_blkcnt_t", "[wrapped_facade_headers][sys_types][blkcnt_t]") {
+    blkcnt_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_fsblkcnt_t", "[wrapped_facade_headers][sys_types][fsblkcnt_t]") {
+    fsblkcnt_t const i{0};
+    REQUIRE(0 == i);
+}
+
+TEST_CASE("test_sys_types_fsfilcnt_t", "[wrapped_facade_headers][sys_types][fsfilcnt_t]") {
+    fsfilcnt_t const i{0};
+    REQUIRE(0 == i);
+}
+}  // namespace ystdlib::wrapped_facade_headers::test
