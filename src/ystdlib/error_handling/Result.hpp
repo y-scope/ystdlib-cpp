d80cf86e1a1f2dae6421978c8ee353408368f424
Xiaochong(Eddy) Wei <40865608+anlowee@users.noreply.github.com>
2025-06-02 18:47:22 -0400

    feat: Replace `outcome` with Boost's version of `outcome` to remove dependence on `quickcpplib`. (#62)
    Co-authored-by: kirkrodrigues <2454684+kirkrodrigues@users.noreply.github.com>

--- src/ystdlib/error_handling/Result.hpp
+++ src/ystdlib/error_handling/Result.hpp
@@ -3,10 +3,10 @@
 
 #include <system_error>
 
-#include <outcome/config.hpp>
-#include <outcome/std_result.hpp>
-#include <outcome/success_failure.hpp>
-#include <outcome/try.hpp>
+#include <boost/outcome/config.hpp>
+#include <boost/outcome/std_result.hpp>
+#include <boost/outcome/success_failure.hpp>
+#include <boost/outcome/try.hpp>
 
 namespace ystdlib::error_handling {
 /**
@@ -20,14 +20,14 @@ namespace ystdlib::error_handling {
  * @tparam ErrorType The type used to represent errors.
  */
 template <typename ReturnType, typename ErrorType = std::error_code>
-using Result = OUTCOME_V2_NAMESPACE::std_result<ReturnType, ErrorType>;
+using Result = BOOST_OUTCOME_V2_NAMESPACE::std_result<ReturnType, ErrorType>;
 
 /**
  * @return A value indicating successful completion of a function that returns a void result (i.e.,
  * `Result<void, E>`).
  */
-[[nodiscard]] inline auto success() -> OUTCOME_V2_NAMESPACE::success_type<void> {
-    return OUTCOME_V2_NAMESPACE::success();
+[[nodiscard]] inline auto success() -> BOOST_OUTCOME_V2_NAMESPACE::success_type<void> {
+    return BOOST_OUTCOME_V2_NAMESPACE::success();
 }
 
 /**
@@ -43,9 +43,9 @@ using Result = OUTCOME_V2_NAMESPACE::std_result<ReturnType, ErrorType>;
  * NOTE: This macro is only supported on GCC and Clang due to reliance on compiler-specific
  * extensions.
  */
-#ifdef OUTCOME_TRYX
+#ifdef BOOST_OUTCOME_TRYX
     // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-    #define YSTDLIB_ERROR_HANDLING_TRYX(expr) OUTCOME_TRYX(expr)
+    #define YSTDLIB_ERROR_HANDLING_TRYX(expr) BOOST_OUTCOME_TRYX(expr)
 #endif
 
 /**
@@ -60,7 +60,7 @@ using Result = OUTCOME_V2_NAMESPACE::std_result<ReturnType, ErrorType>;
  * - Otherwise, execution continues normally.
  */
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define YSTDLIB_ERROR_HANDLING_TRYV(expr) OUTCOME_TRYV(expr)
+#define YSTDLIB_ERROR_HANDLING_TRYV(expr) BOOST_OUTCOME_TRYV(expr)
 }  // namespace ystdlib::error_handling
 
 #endif  // YSTDLIB_ERROR_HANDLING_RESULT_HPP
