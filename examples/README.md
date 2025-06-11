43ee6cc8f26c99381948ada6c6f93c5a0a00fe75
John Hao <johnhaoallwood@gmail.com>
2025-06-10 23:17:30 -0400

    add other targets for examples and a README

---
+++ examples/README.md
@@ -0,0 +1,21 @@
+# Examples
+
+The example program `main` uses ystdlib's 4 different library targets.
+
+## Building
+
+To build the examples, run one of the following task commands
+```shell
+# debug build
+task build:examples-debug
+
+# release build
+task build:examples-release
+```
+
+## Running
+
+Run the example program as follows: it should display "Example is run!" at the end of execution
+```shell
+./examples/build/main
+```
--- examples/main.cpp
+++ examples/main.cpp
@@ -1,9 +1,12 @@
 #include <string>
-#include <ystdlib/error_handling/ErrorCode.hpp>
 #include <cstdint>
 #include <iostream>
+#include <cassert>
 
 #include <ystdlib/error_handling/ErrorCode.hpp>
+#include <ystdlib/containers/Array.hpp>
+#include <ystdlib/io_interface/ReaderInterface.hpp>
+#include <ystdlib/wrapped_facade_headers/sys/types.h>
 
 enum class BinaryErrorCodeEnum : uint8_t {
     Success = 0,
@@ -25,11 +28,30 @@ auto BinaryErrorCategory::name() const noexcept -> char const* {
 YSTDLIB_ERROR_HANDLING_MARK_AS_ERROR_CODE_ENUM(BinaryErrorCodeEnum);
 
 int main() {
-    // Test error codes within the same error category
+    // error_handling
     BinaryErrorCode const success{BinaryErrorCodeEnum::Success};
-
     BinaryErrorCode const failure{BinaryErrorCodeEnum::Failure};
 
-    std::cout << "test is good!" << std::endl;
+    // containers
+    constexpr size_t cBufferSize{1024};
+    Array<size_t> arr(cBufferSize);
+    for (size_t idx{0}; idx < cBufferSize; ++idx) {
+        arr.at(idx) = idx;
+    }
+    auto const& arr_const_ref = arr;
+    assert(std::ranges::equal(arr, arr_const_ref));
+
+    // io_interface
+    ReaderInterface my_reader_interface();
+
+    // wrapped_facade_headers
+    u_int const i{0};
+    assert(0 == i);
+    u_long const i{0};
+    assert(0 == i);
+    quad_t const i{0};
+    assert(0 == i);
+
+    std::cout << "Example is run!" << std::endl;
     return 0;
 }
