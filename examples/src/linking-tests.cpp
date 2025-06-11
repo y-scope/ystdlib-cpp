2dbca54a153cce43336237274c8aea0fb0c28c86
davidlion <david.lion@yscope.com>
2025-06-11 01:45:28 -0400

    Update examples.

---
+++ examples/src/linking-tests.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <exception>
+#include <string_view>
+
+#include <ystdlib/containers/Array.hpp>
+#include <ystdlib/error_handling/ErrorCode.hpp>
+#include <ystdlib/io_interface/ErrorCode.hpp>
+#include <ystdlib/io_interface/ReaderInterface.hpp>
+#include <ystdlib/wrapped_facade_headers/sys/types.h>
+
+enum class BinaryErrorCodeEnum : uint8_t {
+    Success = 0,
+    Failure
+};
+
+using BinaryErrorCode = ystdlib::error_handling::ErrorCode<BinaryErrorCodeEnum>;
+
+using BinaryErrorCategory = ystdlib::error_handling::ErrorCategory<BinaryErrorCodeEnum>;
+
+constexpr std::string_view cBinaryTestErrorCategoryName{"Binary Error Code"};
+
+template <>
+auto BinaryErrorCategory::name() const noexcept -> char const* {
+    return cBinaryTestErrorCategoryName.data();
+}
+
+YSTDLIB_ERROR_HANDLING_MARK_AS_ERROR_CODE_ENUM(BinaryErrorCodeEnum);
+
+using ystdlib::io_interface::ErrorCode;
+
+class FailureReader : ystdlib::io_interface::ReaderInterface {
+public:
+    [[nodiscard]] auto read(char* /*buf*/, size_t /*num_bytes_to_read*/, size_t& /*num_bytes_read*/)
+            -> ErrorCode override {
+        return ErrorCode::ErrorCode_Unsupported;
+    }
+
+    [[nodiscard]] auto seek_from_begin(size_t /*pos*/) -> ErrorCode override {
+        return ErrorCode::ErrorCode_Unsupported;
+    }
+
+    [[nodiscard]] auto seek_from_current(off_t /*offset*/) -> ErrorCode override {
+        return ErrorCode::ErrorCode_Unsupported;
+    }
+
+    [[nodiscard]] auto get_pos(size_t& /*pos*/) -> ErrorCode override {
+        return ErrorCode::ErrorCode_Unsupported;
+    }
+};
+
+auto main() -> int {
+    // error_handling
+    BinaryErrorCode const success{BinaryErrorCodeEnum::Success};
+    BinaryErrorCode const failure{BinaryErrorCodeEnum::Failure};
+
+    // containers
+    constexpr size_t cBufferSize{1024};
+    ystdlib::containers::Array<size_t> arr(cBufferSize);
+    try {
+        for (size_t idx{0}; idx < cBufferSize; ++idx) {
+            arr.at(idx) = idx;
+        }
+        auto const& arr_const_ref = arr;
+        assert(std::ranges::equal(arr, arr_const_ref));
+
+        // io_interface
+        FailureReader reader{};
+        assert(ErrorCode::ErrorCode_Unsupported == reader.seek_from_begin(0));
+
+        // wrapped_facade_headers
+        u_int const uint{0};
+        assert(0 == uint);
+        u_long const ulong{0};
+        assert(0 == ulong);
+        quad_t const quadt{0};
+        assert(0 == quadt);
+    } catch (std::exception const& e) {
+        return 1;
+    }
+
+    return 0;
+}
