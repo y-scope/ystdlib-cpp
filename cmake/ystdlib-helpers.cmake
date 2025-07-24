753e81540e7a4a5cad7e7a6334d740cfdc6dcc9a
davidlion <david.lion@yscope.com>
2025-07-24 17:39:29 -0400

    fix(lint): Update to gersemi 0.21.0 and apply corresponding format changes. (#69)

--- cmake/ystdlib-helpers.cmake
+++ cmake/ystdlib-helpers.cmake
@@ -106,8 +106,7 @@ function(add_cpp_library)
     target_sources(
         ${ARG_NAME}
         PUBLIC
-            FILE_SET
-            HEADERS
+            FILE_SET HEADERS
                 BASE_DIRS
                     "$<BUILD_INTERFACE:${ARG_BUILD_INCLUDE_DIRS}>"
                     "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>"
